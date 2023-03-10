75bbae8e71d869df40d7522535b306aacca4ffe3
EsraaHamada100 <esraahamada654@gmail.com>
2023-03-10 22:12:36 +0200

    conditional handeling

--- src/pages/products/ProductsList.js
+++ src/pages/products/ProductsList.js
@@ -1,40 +1,37 @@
 import ProductCard from "./components/ProductCard";
 import './style/ProductsList.css';
 import { data } from '../../core/data/movies.js';
+import EmptyProduct from "./components/EmptyProduct";
 const ProductsList = () => {
     const moviesData = data;
+    const watchNow = (id) => {
+        console.log('we clicked in video id =', id);
+    }
     //  a function that return movies
     const displayMovies = () => {
-        const watchNow = (id)=>{
-            console.log('we clicked in video id =',id);
+        if (moviesData.length === 0) {
+            return <EmptyProduct />;
         }
-        return moviesData.map(
-            (movie) => {
-                /* Here he will ask you for a unique identifier for the element
-                in order not to reload every element when the list changes
-                 he only want to change the item with the specific id*/
-                //  Here I also send some props it's like parameters that we send to function
-                return < ProductCard 
-                            key={movie.id} 
-                            id={movie.id}
-                            name={movie.name} 
-                            description={movie.description} 
-                            image={movie.image} 
-                            watch={watchNow}
-                        />;
-            }
-        )
+        else {
+            return moviesData.map(
+                (movie) => {
+                    /* Here he will ask you for a unique identifier for the element
+                    in order not to reload every element when the list changes
+                     he only want to change the item with the specific id*/
+                    //  Here I also send some props it's like parameters that we send to function
+                    return < ProductCard
+                        key={movie.id}
+                        id={movie.id}
+                        name={movie.name}
+                        description={movie.description}
+                        image={movie.image}
+                        watch={watchNow}
+                    />;
+                }
+            )
+        }
     }
-    return (
-
-        <div className="products-list">
-            {
-                /* You should but any code inside jsx in {} */
-                displayMovies()
-            }
-
-        </div>
-    );
+    return <div className="products-list">{displayMovies()}</div>;
 }
 
 export default ProductsList;
---
+++ src/pages/products/components/EmptyProduct.js
@@ -0,0 +1,10 @@
+import '../style/EmptyProduct.css';
+const EmptyProduct = () => {
+    return (
+    <div className="empty-product">
+        <p>There is no Movies , go and study something</p>
+    </div>
+    );
+}
+ 
+export default EmptyProduct;
