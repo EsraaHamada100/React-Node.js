b985a74d1dc0141cf6f42b602cb6951c02aabbc4
EsraaHamada100 <esraahamada654@gmail.com>
2023-03-10 21:56:35 +0200

    props [child-to-parent]

--- src/pages/products/ProductsList.js
+++ src/pages/products/ProductsList.js
@@ -4,13 +4,24 @@ import { data } from '../../core/data/movies.js';
 const ProductsList = () => {
     const moviesData = data;
     //  a function that return movies
-    const displayMovies = ()=>{
+    const displayMovies = () => {
+        const watchNow = (id)=>{
+            console.log('we clicked in video id =',id);
+        }
         return moviesData.map(
             (movie) => {
                 /* Here he will ask you for a unique identifier for the element
                 in order not to reload every element when the list changes
                  he only want to change the item with the specific id*/
-                return < ProductCard key={movie.id}/>;
+                //  Here I also send some props it's like parameters that we send to function
+                return < ProductCard 
+                            key={movie.id} 
+                            id={movie.id}
+                            name={movie.name} 
+                            description={movie.description} 
+                            image={movie.image} 
+                            watch={watchNow}
+                        />;
             }
         )
     }
--- src/pages/products/components/ProductCard.js
+++ src/pages/products/components/ProductCard.js
@@ -1,19 +1,22 @@
 import '../style/ProductCard.css';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
     return ( 
         <div className="product-card">
             <div className="card-top">
                 <img 
-                    src='https://picsum.photos/200'
+                    src={props.image}
                     alt='product-card'
                 />
             </div>
             <div className="card-info">
-                <h4 className="title">Black Adam</h4>
-                <p className="info">Lorem ipsum dolor sit amet, consectetur adip,
-                     sed do eiusmod tempor incididunt ut labore et</p>
-                <button>Watch Now</button>
+                <h4 className="title">{props.name}</h4>
+                <p className="info">{props.description}</p>
+                <button onClick={()=>{
+                    props.watch(props.id);
+                }}>
+                    Watch Now
+                </button>
             </div>
         </div>
      );
